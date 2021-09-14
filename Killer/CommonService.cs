664cfeea6c4c3c060dcbc91a9a148897311539fd
Simon Hjorthøj <sh2@live.dk>
2021-09-14 18:21:28 +0200

    Changed CheckIfContextAndCommandIsNull to ContextAndCommandIsNullCheck

--- Killer/CommonService.cs
+++ Killer/CommonService.cs
@@ -25,7 +25,7 @@ namespace KillersLibrary {
         /// <param name="command">The <see cref="SocketSlashCommand"/> is used to send slash commands.</param>
         /// <returns>A DiscordID from either <see cref="SocketCommandContext"/> or <see cref="SocketSlashCommand"/> depending on which is null.</returns>
         public virtual ulong GetDiscordID(SocketCommandContext context = null, SocketSlashCommand command = null) {
-            CheckIfContextAndCommandIsNull(context, command);
+            ContextAndCommandIsNullCheck(context, command);
             if (context == null) return command.User.Id;
             else return context.User.Id;
         }
@@ -38,7 +38,7 @@ namespace KillersLibrary {
         /// <returns>A UserID from either <see cref="SocketCommandContext"/> or <see cref="SocketSlashCommand"/> depending on which is null.</returns>
         [Obsolete("This method will soon be deprecated and will be removed in future versions. Please use the new GetDiscordID instead", true)]
         public virtual ulong GetUserID(SocketCommandContext context = null, SocketSlashCommand command = null) {
-            CheckIfContextAndCommandIsNull(context, command);
+            ContextAndCommandIsNullCheck(context, command);
             if (context == null) return command.User.Id;
             else return context.User.Id;
         }
@@ -50,7 +50,7 @@ namespace KillersLibrary {
         /// <param name="command">The <see cref="SocketSlashCommand"/> is used to send slash commands.</param>
         /// <returns>A GuildID from either <see cref="SocketCommandContext"/> or <see cref="SocketSlashCommand"/> depending on which is null.</returns>
         public virtual ulong GetGuildID(SocketCommandContext context = null, SocketSlashCommand command = null) {
-            CheckIfContextAndCommandIsNull(context, command);
+            ContextAndCommandIsNullCheck(context, command);
             if (context == null) return ((SocketGuildUser)command.User).Guild.Id;
             else return context.Guild.Id;
         }
@@ -62,7 +62,7 @@ namespace KillersLibrary {
         /// <param name="command">The <see cref="SocketSlashCommand"/> is used to send slash commands.</param>
         /// <returns>A AuthorID from either <see cref="SocketCommandContext"/> or <see cref="SocketSlashCommand"/> depending on which is null.</returns>
         public virtual ulong GetAuthorID(SocketCommandContext context = null, SocketSlashCommand command = null) {
-            CheckIfContextAndCommandIsNull(context, command);
+            ContextAndCommandIsNullCheck(context, command);
             if (context == null) return command.User.Id;
             else return context.Message.Author.Id;
         }
@@ -72,7 +72,7 @@ namespace KillersLibrary {
         /// </summary>
         /// <param name="context">The <see cref="SocketCommandContext"/>.</param>
         /// <param name="command">The <see cref="SocketSlashCommand"/>.</param>
-        public void CheckIfContextAndCommandIsNull(SocketCommandContext context = null, SocketSlashCommand command = null) {
+        public void ContextAndCommandIsNullCheck(SocketCommandContext context = null, SocketSlashCommand command = null) {
             if (context == null && command == null) throw new ArgumentException("Both the context and the command is empty. Please fill one of the parameters.");
         }
 
@@ -86,7 +86,7 @@ namespace KillersLibrary {
         /// <param name="context">The <see cref="SocketCommandContext"/> is used to send normal commands.</param>
         /// <param name="command">The <see cref="SocketSlashCommand"/> is used to send slash commands.</param>
         public virtual async Task MakeFileResponse(Stream stream, string filename, string text = null, SocketCommandContext context = null, SocketSlashCommand command = null) {
-            CheckIfContextAndCommandIsNull(context, command);
+            ContextAndCommandIsNullCheck(context, command);
             if (context == null) await command.FollowupWithFileAsync(text, stream, filename);
             else await context.Channel.SendFileAsync(stream, filename, text);
         }
@@ -100,7 +100,7 @@ namespace KillersLibrary {
         /// <param name="context">The <see cref="SocketCommandContext"/> is used to send normal commands.</param>
         /// <param name="command">The <see cref="SocketSlashCommand"/> is used to send slash commands.</param>
         public virtual async Task<RestUserMessage> MakeResponse(string text = null, Embed embed = null, MessageComponent component = null, SocketCommandContext context = null, SocketSlashCommand command = null) {
-            CheckIfContextAndCommandIsNull(context, command);
+            ContextAndCommandIsNullCheck(context, command);
             if (context == null) return await command.FollowupAsync(text, embed: embed, component: component);
             else return await context.Channel.SendMessageAsync(text, embed: embed, component: component);
         }
