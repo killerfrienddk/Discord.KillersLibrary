014b0fa51bb0f2c24229acf57a0b182ed2490ad7
Simon Hjorthøj <sh2@live.dk>
2021-09-20 23:30:13 +0200

    added support for multi message multibuttons

--- Killer/MultiButtonsService.cs
+++ Killer/MultiButtonsService.cs
@@ -6,11 +6,22 @@ using Discord;
 using System.Threading.Tasks;
 
 namespace KillersLibrary {
+
+    /// <summary>
+    ///     This is for choosing the amount of rows pr message.
+    /// </summary>
+    public enum MultiButtonsRows {
+        One = 125,
+        Two = 250,
+        Three = 375,
+        Four = 500,
+        Five = 625
+    }
+
     public class MultiButton {
         public string Title { get; set; }
         public string Value { get; set; }
     }
-
 
     public class BaseStyle {
         public string CustomID { get; set; }
@@ -20,6 +31,11 @@ namespace KillersLibrary {
 
     public class MultiButtonsStyles : BaseStyle {
         public bool UpperCaseLetters { get; set; } = true;
+    }
+
+    public class MultipleMultiButtonsStyles : MultiButtonsStyles {
+        public bool DoNotAddLastButton { get; set; } = false;
+        public MultiButtonsRows MultiButtonsRows = MultiButtonsRows.Five;
     }
 
     public class SelectForMultiButtonsStyles : BaseStyle {
@@ -35,6 +51,10 @@ namespace KillersLibrary {
         /// <param name="styles">The <see cref="MultiButtonsStyles"/> is for customization of many parameters.</param>
         /// <returns>A <see cref="ComponentBuilder"/></returns>
         public virtual ComponentBuilder CreateMultiButtons(List<string> titles, MultiButtonsStyles styles = null) {
+            return CreateMultiButtonsInternal(titles, styles).Item1;
+        }
+
+        private (ComponentBuilder, int buttonCount) CreateMultiButtonsInternal(List<string> titles, MultiButtonsStyles styles = null, int buttonCount = 0) {
             styles ??= new();
 
             if (styles.OrderByTitle) titles = titles.OrderBy(t => t).ToList();
@@ -42,7 +62,6 @@ namespace KillersLibrary {
             var builder = new ComponentBuilder();
             int count = 1;
             int resetAt = 0;
-            int buttonCount = 0;
             string firstLetter = "";
             string lastLetter;
             for (int i = 0; i < titles.Count; i++) {
@@ -54,7 +73,7 @@ namespace KillersLibrary {
                     resetAt = i + 1;
                     ButtonBuilder button = new();
                     string letters = "";
-                    if (string.IsNullOrWhiteSpace(firstLetter)) {
+                    if (string.IsNullOrWhiteSpace(firstLetter) || firstLetter.ToLower() == lastLetter.ToLower()) {
                         letters = styles.UpperCaseLetters ? lastLetter.ToUpper() : lastLetter.ToLower();
                     } else {
                         letters = (styles.UpperCaseLetters ? firstLetter.ToUpper() : firstLetter.ToLower()) +
@@ -91,19 +110,7 @@ namespace KillersLibrary {
                 builder.WithButton(button);
             }
 
-            return builder;
-        }
-
-
-        /// <summary>
-        ///     This is for choosing the amount of rows pr message.
-        /// </summary>
-        public enum MultiButtonsRows {
-            One = 125,
-            Two = 250,
-            Three = 375,
-            Four = 500,
-            Five = 650
+            return (builder, buttonCount);
         }
 
         /// <summary>
@@ -112,12 +119,21 @@ namespace KillersLibrary {
         /// <param name="titles">List of strings that will be placed on the buttons title e.g A-B"/></param>
         /// <param name="styles">The <see cref="MultiButtonsStyles"/> is for customization of many parameters.</param>
         /// <returns>A list of <see cref="ComponentBuilder"/></returns>
-        public virtual List<ComponentBuilder> CreateMultipleMultiButtons(List<string> titles, MultiButtonsRows multiButtonsRows = MultiButtonsRows.Five, MultiButtonsStyles styles = null) {
+        public virtual List<ComponentBuilder> CreateMultipleMultiButtons(List<string> titles, MultipleMultiButtonsStyles styles = null) {
+            styles ??= new();
             List<ComponentBuilder> componentBuilders = new();
 
-            for (int i = 0; i < titles.Count; i++) {
-                var strings = titles.Skip(i + (int)multiButtonsRows).Take((int)multiButtonsRows).ToList();
-                componentBuilders.Add(CreateMultiButtons(strings, styles));
+            if (styles.OrderByTitle) {
+                titles = titles.OrderBy(t => t).ToList();
+                styles.OrderByTitle = false; // Prevets the CreateMultiButtonsInternal from doing it Ordering aswell.
+            }
+
+            int buttonCount = 0;
+            for (int i = 0; i * (int)styles.MultiButtonsRows < titles.Count; i++) {
+                var strings = titles.Skip(i * (int)styles.MultiButtonsRows - (styles.DoNotAddLastButton ? 25 : 0)).Take((int)styles.MultiButtonsRows - (styles.DoNotAddLastButton ? 25 : 0)).ToList();
+                (ComponentBuilder, int) multiButton = CreateMultiButtonsInternal(strings, styles, buttonCount);
+                componentBuilders.Add(multiButton.Item1);
+                buttonCount = multiButton.Item2;
             }
 
             return componentBuilders;
@@ -130,7 +146,6 @@ namespace KillersLibrary {
         /// <param name="multiButtons">List of <see cref="MultiButton"/>s that will be calculated placed in the select depending on the range that has been choosen."/></param>
         /// <param name="styles">The <see cref="SelectForMultiButtonsStyles"/> is for customization of many parameters.</param>
         /// <returns>A <see cref="ComponentBuilder"/></returns>
-        [Obsolete("This method will soon be deprecated and will be removed in future versions. Please use the new version instead CreateSelectForMultiButtonsAsync.", true)]
         public virtual ComponentBuilder CreateSelectForMultiButtons(SocketMessageComponent interaction, List<MultiButton> multiButtons, SelectForMultiButtonsStyles styles = null) {
             styles ??= new();
 
@@ -143,40 +158,7 @@ namespace KillersLibrary {
             if (styles.OrderByTitle) multiButtons = multiButtons.OrderBy(m => m.Title).ToList();
             try {
                 for (int i = (number * 25) - 25; i < number * 25; i++) {
-                    selectMenu.AddOption(multiButtons[i].Title, multiButtons[i].Value);
-                    lastLetter = multiButtons[i].Title[0].ToString();
-                }
-            } catch (Exception) { }
-
-            string rangeLetters = $"{multiButtons[(number * 25) - 25].Title[0]}-{lastLetter}";
-
-            selectMenu.WithPlaceholder(styles.Placeholder +
-                (styles.RagedLettersOnEndOfPlaceholder ? " - " + rangeLetters : ""));
-
-            return new ComponentBuilder().WithSelectMenu(selectMenu);
-        }
-
-        /// <summary>
-        ///     Creates Select For Multi Buttons.
-        /// </summary>
-        /// <param name="interaction">The <see cref="SocketMessageComponent"/> is used for getting the number at the end of the customID. <see cref="SocketMessageComponent"/></param>
-        /// <param name="multiButtons">List of <see cref="MultiButton"/>s that will be calculated placed in the select depending on the range that has been choosen."/></param>
-        /// <param name="styles">The <see cref="SelectForMultiButtonsStyles"/> is for customization of many parameters.</param>
-        /// <returns>A <see cref="ComponentBuilder"/></returns>
-        public virtual async Task<ComponentBuilder> CreateSelectForMultiButtonsAsync(SocketMessageComponent interaction, List<MultiButton> multiButtons, SelectForMultiButtonsStyles styles = null) {
-            await interaction.DeferAsync();
-            styles ??= new();
-
-            int number = Convert.ToInt32(string.Join("", interaction.Data.CustomId.Where(c => char.IsDigit(c))));
-
-            SelectMenuBuilder selectMenu = new SelectMenuBuilder()
-                .WithCustomId(styles.CustomID);
-
-            string lastLetter = "";
-            if (styles.OrderByTitle) multiButtons = multiButtons.OrderBy(m => m.Title).ToList();
-            try {
-                for (int i = (number * 25) - 25; i < number * 25; i++) {
-                    selectMenu.AddOption(multiButtons[i].Title, multiButtons[i].Value);
+                    selectMenu.AddOption(multiButtons[i].Title.Substring(0, multiButtons[i].Title.Length > 25 ? 25 : multiButtons[i].Title.Length), multiButtons[i].Value);
                     lastLetter = multiButtons[i].Title[0].ToString();
                 }
             } catch (Exception) { }
