3a5653f680af0aad48030fbbb81eb01fb40d8776
Simon Hjorthøj <sh2@live.dk>
2021-09-20 20:16:56 +0200

    Added a CreateSelectForMultiButtonsAsync for allowing deferAsync

--- Killer/MultiButtonsService.cs
+++ Killer/MultiButtonsService.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System;
 using Discord.WebSocket;
 using Discord;
+using System.Threading.Tasks;
 
 namespace KillersLibrary {
     public class MultiButton {
@@ -129,7 +130,41 @@ namespace KillersLibrary {
         /// <param name="multiButtons">List of <see cref="MultiButton"/>s that will be calculated placed in the select depending on the range that has been choosen."/></param>
         /// <param name="styles">The <see cref="SelectForMultiButtonsStyles"/> is for customization of many parameters.</param>
         /// <returns>A <see cref="ComponentBuilder"/></returns>
+        [Obsolete("This method will soon be deprecated and will be removed in future versions. Please use the new version instead CreateSelectForMultiButtonsAsync.", true)]
         public virtual ComponentBuilder CreateSelectForMultiButtons(SocketMessageComponent interaction, List<MultiButton> multiButtons, SelectForMultiButtonsStyles styles = null) {
+            styles ??= new();
+
+            int number = Convert.ToInt32(string.Join("", interaction.Data.CustomId.Where(c => char.IsDigit(c))));
+
+            SelectMenuBuilder selectMenu = new SelectMenuBuilder()
+                .WithCustomId(styles.CustomID);
+
+            string lastLetter = "";
+            if (styles.OrderByTitle) multiButtons = multiButtons.OrderBy(m => m.Title).ToList();
+            try {
+                for (int i = (number * 25) - 25; i < number * 25; i++) {
+                    selectMenu.AddOption(multiButtons[i].Title, multiButtons[i].Value);
+                    lastLetter = multiButtons[i].Title[0].ToString();
+                }
+            } catch (Exception) { }
+
+            string rangeLetters = $"{multiButtons[(number * 25) - 25].Title[0]}-{lastLetter}";
+
+            selectMenu.WithPlaceholder(styles.Placeholder +
+                (styles.RagedLettersOnEndOfPlaceholder ? " - " + rangeLetters : ""));
+
+            return new ComponentBuilder().WithSelectMenu(selectMenu);
+        }
+
+        /// <summary>
+        ///     Creates Select For Multi Buttons.
+        /// </summary>
+        /// <param name="interaction">The <see cref="SocketMessageComponent"/> is used for getting the number at the end of the customID. <see cref="SocketMessageComponent"/></param>
+        /// <param name="multiButtons">List of <see cref="MultiButton"/>s that will be calculated placed in the select depending on the range that has been choosen."/></param>
+        /// <param name="styles">The <see cref="SelectForMultiButtonsStyles"/> is for customization of many parameters.</param>
+        /// <returns>A <see cref="ComponentBuilder"/></returns>
+        public virtual async Task<ComponentBuilder> CreateSelectForMultiButtonsAsync(SocketMessageComponent interaction, List<MultiButton> multiButtons, SelectForMultiButtonsStyles styles = null) {
+            await interaction.DeferAsync();
             styles ??= new();
 
             int number = Convert.ToInt32(string.Join("", interaction.Data.CustomId.Where(c => char.IsDigit(c))));
