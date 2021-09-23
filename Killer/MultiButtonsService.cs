b4e36768d34bebafd0464083717d5c37aeee5448
Simon Hjorthøj <sh2@live.dk>
2021-09-23 15:37:35 +0200

    Made check on if titles.Count is larger than 625

--- Killer/MultiButtonsService.cs
+++ Killer/MultiButtonsService.cs
@@ -53,6 +53,9 @@ namespace KillersLibrary {
         /// <param name="styles">The <see cref="MultiButtonsStyles"/> is for customization of many parameters.</param>
         /// <returns>A <see cref="ComponentBuilder"/></returns>
         public virtual ComponentBuilder CreateMultiButtons(List<string> titles, MultiButtonsStyles styles = null) {
+            if (titles.Count > (int)MultiButtonsRows.Five) {
+                throw new ArgumentException($"If you have more than {(int)MultiButtonsRows.Five} then please use the {nameof(CreateMultipleMultiButtons)} function.");
+            }
             return CreateMultiButtonsInternal(titles, styles).Item1;
         }
 
