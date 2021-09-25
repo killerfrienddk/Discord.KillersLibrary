78b1a4de1d72d978b1b6c0f47cf6a721c9fae3ee
Simon Hjorthøj <sh2@live.dk>
2021-09-25 20:21:04 +0200

    Added stuff

--- Killer/Services/EmbedPagesService.cs
+++ Killer/Services/EmbedPagesService.cs
@@ -30,8 +30,8 @@ namespace KillersLibrary.Services {
             ComponentBuilder componentBuilder = GetComponentBuilder(styles, extraButtons);
 
             int currentPage = 0;
-            if (styles.PageNumbers) embedBuilders[0] = embedBuilders[0].WithFooter("Page: " + (currentPage + 1) + "/" + embedBuilders.Count);
-            var currentMessage = await CommonService.Instance.MakeResponseAsync(embed: embedBuilders[0].Build(), component: componentBuilder.Build(), context: context, command: command);
+            if (styles.PageNumbers) embedBuilders[0] = embedBuilders[0].WithFooter($"Page: {currentPage + 1}/{embedBuilders.Count}");
+            var currentMessage = await CommonService.Instance.MakeResponse(embed: embedBuilders[0].Build(), component: componentBuilder.Build(), context: context, command: command);
             client.InteractionCreated += async (socketInteraction) => {
                 SocketMessageComponent interaction = (SocketMessageComponent)socketInteraction;
                 if (interaction.Data.Type != ComponentType.Button) return;
@@ -80,16 +80,16 @@ namespace KillersLibrary.Services {
 
             switch (interaction.Data.CustomId) {
                 case "killer_first_embed":
-                case "back_button_embed":
-                case "forward_button_embed":
-                case "last_embed":
-                    if (styles.PageNumbers) embedBuilders[currentPage] = embedBuilders[currentPage].WithFooter("Page: " + (currentPage + 1) + "/" + embedBuilders.Count);
+                case "killer_back_button_embed":
+                case "killer_forward_button_embed":
+                case "killer_last_embed":
+                    if (styles.PageNumbers) embedBuilders[currentPage] = embedBuilders[currentPage].WithFooter($"Page: {currentPage + 1}/{embedBuilders.Count}");
                     await currentMessage.ModifyAsync(msg => {
                         msg.Embed = embedBuilders[currentPage].Build();
                         msg.Components = componentBuilder.Build();
                     });
                     break;
-                case "delete_embed_pages":
+                case "killer_delete_embed_pages":
                     await currentMessage.DeleteAsync();
                     await interaction.FollowupAsync(styles.DeletionMessage, ephemeral: true);
                     break;
@@ -100,18 +100,18 @@ namespace KillersLibrary.Services {
 
         private int GetCurrentPage(SocketMessageComponent interaction, int currentPage, List<EmbedBuilder> embedBuilders) {
             switch (interaction.Data.CustomId) {
-                case "back_button_embed":
+                case "killer_back_button_embed":
                     if (currentPage - 1 < 0) currentPage = embedBuilders.Count - 1;
                     else currentPage -= 1;
                     break;
-                case "forward_button_embed":
+                case "killer_forward_button_embed":
                     if (currentPage + 1 == embedBuilders.Count) currentPage = 0;
                     else currentPage += 1;
                     break;
-                case "last_embed":
+                case "killer_last_embed":
                     currentPage = embedBuilders.Count - 1;
                     break;
-                case "first_embed":
+                case "killer_first_embed":
                     currentPage = 0;
                     break;
             }
@@ -123,36 +123,40 @@ namespace KillersLibrary.Services {
             int buttonCount = 3;
             ComponentBuilder componentBuilder = new();
             if (styles.FastChangeBtns) {
-                ButtonBuilder firstbtn = new ButtonBuilder()
-                    .WithCustomId("first_embed")
+                ButtonBuilder firstBtn = new ButtonBuilder()
+                    .WithCustomId("killer_first_embed")
                     .WithLabel(styles.FirstLabel ?? "«")
+                    .WithStyle(styles.SkipColor);
+                componentBuilder.WithButton(firstBtn);
                     .WithStyle(styles.Skipcolor);
                 componentBuilder.WithButton(firstbtn);
                 buttonCount++;
             }
 
             ButtonBuilder pageMovingButtons2 = new ButtonBuilder()
-                .WithCustomId("back_button_embed")
+                .WithCustomId("killer_back_button_embed")
                 .WithLabel(styles.BackLabel ?? "‹")
-                .WithStyle(styles.Btncolor);
+                .WithStyle(styles.BtnColor);
             componentBuilder.WithButton(pageMovingButtons2);
 
             ButtonBuilder deleteBtn = new ButtonBuilder()
-                .WithCustomId("delete_embed_pages")
+                .WithCustomId("killer_delete_embed_pages")
                 .WithEmote(new Emoji(styles.DelEmoji ?? "🗑"))
                 .WithStyle(ButtonStyle.Danger);
             componentBuilder.WithButton(deleteBtn);
 
             ButtonBuilder pageMovingButtons1 = new ButtonBuilder()
-                .WithCustomId("forward_button_embed")
+                .WithCustomId("killer_forward_button_embed")
                 .WithLabel(styles.ForwardLabel ?? "›")
-                .WithStyle(styles.Btncolor);
+                .WithStyle(styles.BtnColor);
             componentBuilder.WithButton(pageMovingButtons1);
 
             if (styles.FastChangeBtns) {
-                ButtonBuilder lastbtn = new ButtonBuilder()
-                    .WithCustomId("last_embed")
+                ButtonBuilder lastBtn = new ButtonBuilder()
+                    .WithCustomId("killer_last_embed")
                     .WithLabel(styles.LastLabel ?? "»")
+                    .WithStyle(styles.SkipColor);
+                componentBuilder.WithButton(lastBtn);
                     .WithStyle(styles.Skipcolor);
                 componentBuilder.WithButton(lastbtn);
                 buttonCount++;
@@ -174,93 +178,10 @@ namespace KillersLibrary.Services {
         public string ForwardLabel { get; set; } = "›";
         public string LastLabel { get; set; } = "»";
         public string DeletionMessage { get; set; } = "Embed page has been deleted";
-        public ButtonStyle Btncolor { get; set; } = ButtonStyle.Success;
+        public ButtonStyle BtnColor { get; set; } = ButtonStyle.Success;
         public ButtonStyle Delcolor { get; set; } = ButtonStyle.Danger;
-        public ButtonStyle Skipcolor { get; set; } = ButtonStyle.Primary;
+        public ButtonStyle SkipColor { get; set; } = ButtonStyle.Primary;
         public bool FastChangeBtns { get; set; } = false;
         public bool PageNumbers { get; set; } = true;
-    }
-
-    public enum ColorCodes : uint {
-        Default = 0x7F7F7F,
-        Black_Absolute = 0x000000,
-        Black = 0x0C0C0C,
-        Grey_Dark = 0x3F3F3F,
-        Grey = 0x7F7F7F,
-        Grey_Light = 0xD3D3D3,
-        White = 0xF2F2F2,
-        White_Absolute = 0xFFFFFF,
-        Red_Light = 0xFF3333,
-        Red = 0xFF0000,
-        Red_Dark = 0x8C0000,
-        Green_Light = 0x8EED8E,
-        Green = 0x007F00,
-        Green_Dark = 0x006300,
-        Blue_Light = 0xADD8E5,
-        Blue = 0x0000FF,
-        Blue_Dark = 0x00008C,
-        Yellow_Light = 0xFF0000,
-        Yellow = 0xFFCC00,
-        Yellow_Dark = 0xFFFFCC,
-        Orange_Light = 0xD8A366,
-        Orange = 0xFFA500,
-        Orange_Dark = 0xFF8C00,
-        Purple_Light = 0x9370DB,
-        Purple = 0xA021EF,
-        Purple_Dark = 0x663399,
-        Pink_Light = 0x000000,
-        Pink = 0xFFBFCC,
-        Pink_Dark = 0xE8547F,
-        Cyan_Light = 0xE0FFFF,
-        Cyan = 0x00FFFF,
-        Cyan_Dark = 0x008C8C,
-        Magenta_Light = 0xFF77FF,
-        Magenta = 0xFF00FF,
-        Magenta_Dark = 0x8C008C,
-        Brown_Light = 0xAF7F51,
-        Brown = 0xA52828,
-        Brown_Dark = 0x5B3F33,
-        Violet_Light = 0x7A5199,
-        Violet = 0xED82ED,
-        Violet_Dark = 0x9300D3,
-        Turquoise_Light = 0xAFE2DD,
-        Turquoise = 0x30D6C6,
-        Turquoise_Dark = 0x00CED1,
-        Mustard_Light = 0xEDDD60,
-        Mustard = 0xFFDB59,
-        Mustard_Dark = 0x7C7C3F,
-        Ivory_Light = 0xFFF7C9,
-        Ivory = 0xFFFFEF,
-        Ivory_Dark = 0xF2E58E,
-        Gold_Light = 0xF2E5AA,
-        Gold = 0xFFD600,
-        Gold_Dark = 0xEDBC1C,
-        Silver_Light = 0xE0E0E0,
-        Silver = 0xBFBFBF,
-        Silver_Dark = 0xAFAFAF,
-        Silver_Crayola = 0xC9C0BB,
-        Silver_Pink = 0xC4AEAD,
-        Silver_Sand = 0xBFC1C2,
-        Silver_Chalice = 0xACACAC,
-        Silver_Roman = 0x838996,
-        Silver_Old = 0x848482,
-        Silver_Sonic = 0x757575,
-        Bronze = 0xCD7F32,
-        Bronze_BlastOff = 0xA57164,
-        Bronze_Antique = 0x665D1E,
-        Lime = 0x00FF00,
-        Teal = 0x007F7F,
-        Aquamarine = 0x7FFFD3,
-        Aqua = 0x00FFFF,
-        Beige = 0xF4F4DB,
-        Crimson = 0xDB143D,
-        Coral = 0xFF7F4F,
-        Deep_Sky_Blue = 0x00BFFF,
-        Dodger_Blue = 0x1E90FF,
-        Firebrick = 0xB22222,
-        Fuchsia = 0xFF00FF,
-        Indigo = 0x4B0082,
-        Light_Sky_Blue = 0x87CEFA,
-        Maroon = 0xB03060,
     }
 }
