83577c91a9a436d767b1cd7bc4e534a0ba3d5d86
Simon Hjorthøj <sh2@live.dk>
2021-09-25 18:25:28 +0200

    Added Extension and ColorCodes enum

---
+++ Killer/Extensions/EmbedBuilder.cs
@@ -0,0 +1,10 @@
+﻿using Discord;
+using KillersLibrary.Enums;
+
+namespace KillersLibrary.Extensions {
+    public static class EmbedBuilderExtension {
+        public static void WithColorCode(this EmbedBuilder embedBuilder, ColorCodes colorCodes) {
+            embedBuilder.WithColor(new Color((uint)colorCodes));
+        }
+    }
+}
