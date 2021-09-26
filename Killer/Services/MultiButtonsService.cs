b67d0c5093f3f125c329eae8d67f3a91eb81406a
Simon Hjorthøj <sh2@live.dk>
2021-09-26 13:19:00 +0200

    Fixed warning

--- Killer/Services/MultiButtonsService.cs
+++ Killer/Services/MultiButtonsService.cs
@@ -53,7 +53,7 @@ namespace KillersLibrary.Services {
         /// <param name="styles">The <see cref="MultiButtonsStyles"/> is for customization of many parameters.</param>
         /// <returns>A <see cref="ComponentBuilder"/></returns>
         public virtual ComponentBuilder CreateMultiButtons(List<string> titles, MultiButtonsStyles styles = null) {
-            Preconditions.LessThan(titles.Count, (int)MultiButtonsRows.Five, $"If you have more than {(int)MultiButtonsRows.Five} then please use the {nameof(CreateMultipleMultiButtons)} function.");
+            Preconditions.LessThan(titles.Count, (int)MultiButtonsRows.Five, "Buttons", msg: $"If you have more than {(int)MultiButtonsRows.Five} then please use the {nameof(CreateMultipleMultiButtons)} function.");
             return CreateMultiButtonsInternal(titles, styles).Item1;
         }
 
