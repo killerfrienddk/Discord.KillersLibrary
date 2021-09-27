e21a5df539e3716fa66754133d04bde491c83eb7
Simon Hjorthøj <sh2@live.dk>
2021-09-27 15:15:26 +0200

    Changed WithColorCode to return the embedbuilder to allow chaining

--- Killer/Extensions/EmbedBuilder.cs
+++ Killer/Extensions/EmbedBuilder.cs
@@ -3,8 +3,10 @@ using KillersLibrary.Enums;
 
 namespace KillersLibrary.Extensions {
     public static class EmbedBuilderExtension {
-        public static void WithColorCode(this EmbedBuilder embedBuilder, ColorCodes colorCodes) {
+        public static EmbedBuilder WithColorCode(this EmbedBuilder embedBuilder, ColorCodes colorCodes) {
             embedBuilder.WithColor(new Color((uint)colorCodes));
+            return embedBuilder;
+        }
         }
     }
 }
