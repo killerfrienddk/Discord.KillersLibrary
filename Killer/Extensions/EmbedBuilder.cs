42f1114b137f296c5b123a78026f316922d40d6c
Simon Hjorthøj <sh2@live.dk>
2021-09-27 20:05:05 +0200

    Added summaries

--- Killer/Extensions/EmbedBuilder.cs
+++ Killer/Extensions/EmbedBuilder.cs
@@ -3,11 +3,15 @@ using KillersLibrary.Enums;
 
 namespace KillersLibrary.Extensions {
     public static class EmbedBuilderExtension {
-        public static EmbedBuilder WithColorCode(this EmbedBuilder embedBuilder, ColorCodes colorCodes) {
+        /// <summary> Adds embed color based on the provided <see cref="ColorCodes"/>.</summary>
+        /// <param name="colorCodes">Choose which color you want using <see cref="ColorCodes"/></param>
+        public static EmbedBuilder WithColorCode(this EmbedBuilder embedBuilder, ColorCodes colorCodes = ColorCodes.Default) {
             embedBuilder.WithColor(new Color((uint)colorCodes));
             return embedBuilder;
         }
 
+        /// <summary> Adds embed color based on the provided hex code e.g #000000.</summary>
+        /// <param name="hexCode">Choose which color using a hex code.</param>
         public static EmbedBuilder WithHexColor(this EmbedBuilder embedBuilder, string hexCode) {
             bool success = uint.TryParse("0x" + hexCode.Replace("#", ""), out uint hex);
 
