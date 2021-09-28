a03d3f734d963b73322d8a11d796dba4626f5824
Simon Hjorthøj <sh2@live.dk>
2021-09-28 16:25:55 +0200

    Added enum for CodingLanguages

---
+++ Killer/Enums/CodingLanguages.cs
@@ -0,0 +1,83 @@
+﻿namespace KillersLibrary.Enums {
+    //Got this from True Love he allowed me to copy it and modified it.
+    public enum CodingLanguages {
+        actionscript3,
+        apache,
+        applescript,
+        asp,
+        brainfuck,
+        c,
+        cfm,
+        clojure,
+        cmake,
+        /// <summary>CoffeeScript</summary>
+        coffeescript,
+        /// <summary>CoffeeScript</summary>
+        coffee,
+        /// <summary>C++</summary>
+        cpp,
+        cs,
+        csharp,
+        css,
+        csv,
+        bash,
+        diff,
+        elixir,
+        /// <summary>HTML + Embedded Ruby</summary>
+        erb,
+        go,
+        haml,
+        http,
+        java,
+        javascript,
+        json,
+        jsx,
+        less,
+        lolcode,
+        /// <summary>Makefile</summary>
+        make,
+        markdown,
+        matlab,
+        nginx,
+        objectivec,
+        pascal,
+        PHP,
+        Perl,
+        python,
+        /// <summary> Python profiler output</summary>
+        profile,
+        rust,
+        /// <summary>Salt</summary>
+        salt,
+        /// <summary>Salt</summary>
+        saltstate,
+        /// <summary>Shell scripting</summary>
+        shell,
+        /// <summary>Shell scripting</summary>
+        sh,
+        /// <summary>Shell scripting</summary>
+        zsh,
+        /// <summary>Shell scripting</summary>
+        scss,
+        sql,
+        svg,
+        swift,
+        /// <summary>Ruby</summary>
+        rb,
+        /// <summary>Ruby</summary>
+        jruby,
+        /// <summary>Ruby</summary>
+        ruby,
+        smalltalk,
+        /// <summary>Vim Script</summary>
+        vim,
+        /// <summary>Vim Script</summary>
+        viml,
+        volt,
+        vhdl,
+        vue,
+        /// <summary>XML and also used for HTML with inline CSS and Javascript</summary>
+        xml,
+        yaml
+    }
+}
